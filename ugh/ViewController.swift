e960b0c2a4aea988331e99381e6f085c1e09e113
trivector1721 <trivector1721@users.noreply.github.com>
2017-05-23 00:12:54 -0400

    Initial Commit

---
+++ ugh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ugh
+//
+//  Created by Carly St.Pierre on 5/23/17.
+//  Copyright © 2017 Carly St.Pierre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
